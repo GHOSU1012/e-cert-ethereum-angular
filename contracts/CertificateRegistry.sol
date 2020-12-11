9f23f30c8e1861331a58e698ae3c73bbb022fe16
GHOSU1012 <ghosu1012@gmail.com>
2020-12-11 17:44:14 +0100

    UI Update

--- contracts/CertificateRegistry.sol
+++ contracts/CertificateRegistry.sol
@@ -8,12 +8,14 @@ contract CertificateRegistry is Ownable {
     /**
      * The Certificate Document info structure: every certificate info is composed of:
      * - documentHash: Hash of the document generated off-chain
-     * - from: account of the institution that issue the document
+     * - issuer: account of the institution that issue the document
+     * - timeOfIssue - time the document hash was stored.
     * - isStored - a flag to show that a hash is stored or not.
 
      */
     struct DocumentInfo {
         address issuer;
+        string documentHash;
         uint256 timeOfIssue;
         bool isStored;
     }
@@ -22,16 +24,17 @@ contract CertificateRegistry is Ownable {
       A mapping of the document hash to the documentinfo that was issued
       This mapping is used to keep track of every certification document initiated for every student by an educator.
      */
-    mapping(bytes32 => DocumentInfo) public documentRegistry;
+    mapping(string => DocumentInfo) public documentRegistry;
 
     // event for EVM logging
     event LogNewHashStored(
         address indexed issuer,
+        string documentHash,
         uint256 timeOfIssue,
         bool isStored
     );
 
-    modifier onlyNotHashed(bytes32 _documentHash) {
+    modifier onlyNotHashed(string memory _documentHash) {
         require(
             !isHashStored(_documentHash),
             "Error: Only hashes that have not been hashed can be stored"
@@ -39,14 +42,9 @@ contract CertificateRegistry is Ownable {
         _;
     }
 
-    modifier transactionDoesNotExist(bytes32 scriptHash) {
-        require(transactions[scriptHash].value == 0, "Transaction exists");
-        _;
-    }
-
-    modifier onlyHashValueNotEmpty(bytes32 _documentHash) {
+    modifier onlyHashValueNotEmpty(string memory _documentHash) {
         require(
-            _documentHash.length > 0,
+            bytes(_documentHash).length > 0,
             "Error: Error: hash value should not be empty"
         );
         _;
@@ -57,7 +55,7 @@ contract CertificateRegistry is Ownable {
      * The sender of message call is the educator itself
      * @param {string} _documentHash: Hash of the sutuednt submitted for storage on-chain
      */
-    function storeHash(bytes32 _documentHash)
+    function storeHash(string memory _documentHash)
         public
         onlyOwner
         onlyHashValueNotEmpty(_documentHash)
@@ -68,6 +66,7 @@ contract CertificateRegistry is Ownable {
             "Error: ensure educator address exist"
         );
         DocumentInfo memory docInfo = DocumentInfo({
+            documentHash: _documentHash,
             issuer: msg.sender,
             timeOfIssue: block.timestamp,
             isStored: true
@@ -76,17 +75,45 @@ contract CertificateRegistry is Ownable {
         documentRegistry[_documentHash] = docInfo;
 
         // creates the event, to be used to query all the store hash of the certificates
-        emit LogNewHashStored(msg.sender, block.timestamp, true);
+        emit LogNewHashStored(msg.sender, _documentHash, block.number, true);
     }
 
-    function verifyHash(bytes32 _documenteHash) public view returns (bool) {
-        bool valid = isHashStored(_documenteHash);
-        if (valid) {
+    function verifyCertificate(string memory _documenteHash)
+        public
+        view
+        onlyHashValueNotEmpty(_documenteHash)
+        returns (bool)
+    {
+        bool test = stringsEqual(
+            documentRegistry[_documenteHash].documentHash,
+            _documenteHash
+        );
+        if (test) {
             return true;
         }
+        return false;
     }
 
-    function isHashStored(bytes32 _documentHash) internal view returns (bool) {
+    function isHashStored(string memory _documentHash)
+        internal
+        view
+        returns (bool)
+    {
         return documentRegistry[_documentHash].isStored == true;
     }
+
+    function stringsEqual(string storage _a, string memory _b)
+        internal
+        view
+        returns (bool)
+    {
+        bytes storage a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if (a.length != b.length) {
+            return false;
+        }
+        return true;
+    }
+
+    // Note, swithc string to bytes32 to reduce gas cost if the file is hashed with institutionHash = keccak256(abi.encodePacked(block.number, now, msg.data));
 }
